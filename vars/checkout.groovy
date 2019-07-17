1a0cde5ca504432ffaf442668d03a2776a422729
wintoo <wintoo143@gmail.com>
2019-07-16 22:03:38 -0500

    Refactored Jenkinsfile

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,9 @@
+def repoUrl = "https://github.com/wintoo/maven-simple.git"
+def BRANCH_NAME = 'dev'
+
 def call(){
     stage('SCM Checkout'){
-        echo 'Checking out repo'
+        echo "Building branch ${env.BRANCH_NAME}"
+        git url: repoUrl, branch: BRANCH_NAME
     }
-}
+}   
